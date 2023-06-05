08f0cc6451254fc73f4e0b0efd42ed8fe2b09dea
RomanMorozenko <romanvm2020@gmail.com>
2023-06-06 00:20:44 +0700

    todolist basic ui + tasks CRUD

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,118 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import {v4} from "uuid";
+
 import './App.css';
 
+type TaskType = {
+    id:string
+    text:string
+    completed:boolean
+}
+
 function App() {
+    const [tasks,setTasks] = useState<Array<TaskType>>([
+        {
+            id:v4(),
+            text:'Buy some bread',
+            completed:false
+        },
+        {
+            id:v4(),
+            text:'Feed the cat',
+            completed:true
+        },
+        {
+            id:v4(),
+            text:'Work on the project',
+            completed:false
+        },
+    ]);
+
+    const handleAddNewTask = (text:string) => {
+        const newTask:TaskType = {
+            id:v4(),
+            text:text,
+            completed:false
+        }
+        setTasks([...tasks,newTask])
+    }
+
+    const handleTaskStatusChange = (id:string) => {
+        const changedTasks = [...tasks]
+        let task = changedTasks.find(task=>task.id===id);
+        task && (task.completed = !task.completed);
+        setTasks(changedTasks);
+    }
+
+    const handleDeleteTask = (id:string) => {
+        const changedTasks = tasks.filter(task=>task.id !== id)
+        setTasks(changedTasks);
+    }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <TodoList
+            tasks={tasks}
+            handleTaskStatusChange={handleTaskStatusChange}
+            handleDeleteTask={handleDeleteTask}
+            handleAddNewTask={handleAddNewTask}
+        />
     </div>
   );
 }
 
 export default App;
+
+type TodoListProps = {
+    tasks:Array<TaskType>
+    handleTaskStatusChange:(id:string)=>void
+    handleDeleteTask:(id:string)=>void
+    handleAddNewTask:(text:string)=>void
+}
+export const TodoList = (props:TodoListProps) => {
+
+    const [newTaskText,setNewTaskText] = useState('');
+
+    const onAddTaskClick = (e:React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        e.preventDefault()
+        props.handleAddNewTask(newTaskText);
+        setNewTaskText('');
+    };
+
+    return (
+        <div>
+            <form>
+                <input
+                    type='text'
+                    value={newTaskText}
+                    onChange={e=>setNewTaskText(e.target.value)}
+                />
+                <button
+                    onClick={e=>onAddTaskClick(e)}
+                >+</button>
+            </form>
+            <ul>
+                {props.tasks.map((task:TaskType)=>{
+                    return <li
+                        key={task.id}
+                    >
+                        <input
+                            type="checkbox"
+                            checked={task.completed}
+                            onChange={()=>props.handleTaskStatusChange(task.id)}
+                        />
+                        {task.text}
+                        <button
+                            onClick={()=>props.handleDeleteTask(task.id)}
+                        >
+                            Delete
+                        </button>
+                    </li>
+                })}
+            </ul>
+        </div>
+    )
+}
+
+
